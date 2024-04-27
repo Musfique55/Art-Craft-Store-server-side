d502fb77acd49468d19b825c7811d38ed242cbbb
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-04-27 22:28:54 +0600

    made search by email api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -24,22 +24,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const craftcollections = client.db("craftDB").collection("craft");
 
     app.get('/crafts', async(req,res) => {
-        const email = req.params.email;
+        const email = req.body.email;
         if(email){
             const query = {email : email};
             console.log(query);
             const cursor = craftcollections.find(query);
-            // const result = await cursor.toArray();
-            // res.send(result);
+            const result = await cursor.toArray();
+            res.send(result);
         } else{
             const cursor = craftcollections.find();
             const result = await cursor.toArray();
             res.send(result);
         }
+    })
+
+    app.get('/crafts/:category', async(req,res) => {
+        const category = req.params.category;
+        const query = {subcategory : category}
+        const result = await craftcollections.find(query).toArray();
+        res.send(result);
     })
     app.get('/crafts/:id',async(req,res) => {
         const id = req.params.id;
@@ -47,13 +54,13 @@ async function run() {
         const result = await craftcollections.findOne(query);
         res.send(result);
     })
-    // app.get('/crafts/:email',async(req,res) => {
-    //     const email = req.params.email;
-    //     const query = {email : email};
-    //     const cursor = craftcollections.find(query);
-    //     const result = await cursor.toArray();
-    //     res.send(result);
-    // })
+    app.get('/craft/:email',async(req,res) => {
+        const email = req.params.email;
+        const query = {email : email};
+        const cursor = craftcollections.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
     app.post('/crafts', async(req,res) => {
         const craft = req.body;
         const result = await craftcollections.insertOne(craft);
